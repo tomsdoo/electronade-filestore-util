f67187d7c4474842b92f10918f1edfe0d778cbd7
tom <toms@live.jp>
2022-08-28 15:08:32 +0900

    test for FileStore

---
+++ test/test.ts
@@ -0,0 +1,53 @@
+import { describe, it } from "mocha";
+import { strict as assert } from "assert";
+
+import { FileStore } from "../src/";
+
+describe("FileStore class interfaces", () => {
+  before(() => {
+    // @ts-ignore
+    globalThis.electronade = {
+      filestore: {
+        get: (filePath: string, id: string) =>
+          Promise.resolve({ _id: id, filePath }),
+        save: (filePath: string, item: object) =>
+          Promise.resolve({
+            ...item,
+             _id: "dummy",
+             filePath
+           }),
+        remove: (filePath: string, id: string) =>
+          Promise.resolve(undefined)
+      }
+    };
+  });
+
+  it("get()", async () => {
+    assert.equal(
+      await new FileStore("test")
+        .get("dummyId")
+        .then(result => JSON.stringify(result)),
+      JSON.stringify({
+        _id: "dummyId",
+        filePath: "test"
+      })
+    )
+  });
+
+  it("save()", async () => {
+    assert.equal(
+      await new FileStore("test")
+        .save({ name: "test" })
+        .then(({ name }) => name),
+      "test"
+    );
+  });
+
+  it("remove()", async () => {
+    assert.equal(
+      await new FileStore("test")
+        .remove("dummyId"),
+      undefined
+    );
+  });
+});
