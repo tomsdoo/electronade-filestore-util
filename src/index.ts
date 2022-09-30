8233675fa486c6c9b650c1a1b92c10bed1a2d10b
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-01 07:34:45 +0900

    eslint is added (#4)
    * eslint is installed
    
    * npm script lint:src is added
    
    * lint:src fix
    
    * npm script lint:test is added
    
    * lint:test fix

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,26 @@ export class FileStore {
   protected exposedName: string;
   constructor(filePath: string, exposedName?: string) {
     this.filePath = filePath;
-    this.exposedName = exposedName || "electronade";
+    this.exposedName = exposedName ?? "electronade";
   }
+
   public async get(id: string): Promise<any> {
-    // @ts-ignore
+    // @ts-expect-error
     return globalThis[this.exposedName].filestore.get(this.filePath, id);
   }
+
   public async getIds(): Promise<string[]> {
-    // @ts-ignore
+    // @ts-expect-error
     return globalThis[this.exposedName].filestore.getIds(this.filePath);
   }
+
   public async save(item: object): Promise<any> {
-    // @ts-ignore
+    // @ts-expect-error
     return globalThis[this.exposedName].filestore.save(this.filePath, item);
   }
+
   public async remove(id: string): Promise<undefined> {
-    // @ts-ignore
+    // @ts-expect-error
     return globalThis[this.exposedName].filestore.remove(this.filePath, id);
   }
 }
