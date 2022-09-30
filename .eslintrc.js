8b4c0c6927ba49b9488e906e90009abb9f13107b
tom <toms@live.jp>
2022-10-01 07:29:43 +0900

    eslint is installed

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.base.json'
+  },
+  rules: {
+  }
+}
